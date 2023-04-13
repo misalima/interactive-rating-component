cce1049680d1e08debd79a924edf40b990226a91
Misael Lima <misael.alisson14@gmail.com>
2023-04-13 06:30:55 -0300

    finish styling component in first state

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,16 @@ import TheWelcome from './components/TheWelcome.vue'
       <h1>How did we do?</h1>
       <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
       <div class="inputs-div">
-        <input type="checkbox" name="1-star" id="">
+        <input type="radio" name="rating" id="1-star" value="1">
         <label for="1-star">1</label>
-        <input type="checkbox" name="2-star" id="">
-        <label for="1-star">2</label>
-        <input type="checkbox" name="3-star" id="">
-        <label for="1-star">3</label>
-        <input type="checkbox" name="4-star" id="">
-        <label for="1-star">4</label>
-        <input type="checkbox" name="5-star" id="">
-        <label for="1-star">5</label>
+        <input type="radio" name="rating" id="2-star" value="2">
+        <label for="2-star">2</label>
+        <input type="radio" name="rating" id="3-star" value="3">
+        <label for="3-star">3</label>
+        <input type="radio" name="rating" id="4-star" value="4">
+        <label for="4-star">4</label>
+        <input type="radio" name="rating" id="5-star" value="5">
+        <label class="selected" for="5-star">5</label>
       </div>
       <button>SUBMIT</button>
         
